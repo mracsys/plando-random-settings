34592cf56a874eebb217b218cc9a3b10ffa5579e
mracsys <mracsys@gmail.com>
2022-05-09 19:10:48 -0400

    update upstream randomizer

--- version.py
+++ version.py
@@ -2,5 +2,5 @@ __version__ = "2.2.10-M"
 version_hash_1 = "Mirror Shield"
 version_hash_2 = "Mirror Shield"
 
-randomizer_version = '6.2.72 M-8'
-randomizer_commit = '82f2656931c8a1c6ec784efd19ce21dc107dfb06'
+randomizer_version = '6.2.72 M-10'
+randomizer_commit = 'c6557a4e1a212889cc8e3dc40f7897416bb83239'
