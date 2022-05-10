21671cbdcd5a33e921bced25a58a6279e8f4738d
mracsys <mracsys@gmail.com>
2022-05-09 22:25:43 -0400

    update beehives

--- version.py
+++ version.py
@@ -2,5 +2,5 @@ __version__ = "2.2.10-M"
 version_hash_1 = "Mirror Shield"
 version_hash_2 = "Mirror Shield"
 
-randomizer_version = '6.2.72 M-10'
-randomizer_commit = 'c6557a4e1a212889cc8e3dc40f7897416bb83239'
+randomizer_version = '6.2.72 M-11'
+randomizer_commit = '5b5496ef191d7561b6c177ae2d6fc53afad96674'
